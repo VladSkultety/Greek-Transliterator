611805fbf5640db5f54785ab66c8feef14933423
VladSkultety <vladimir.skultety@protonmail.com>
2021-02-01 17:40:48 +0100

    Main.py finalization (almost). readme.md file update.

--- Main.py
+++ Main.py
@@ -1,9 +1,33 @@
 #coding: utf-8 -*-
-import readline #allows for raw_input to handle larger texts
 
-text = raw_input('Input Greek Text: ')
+GreekText = open("GreekText.txt", "r")
+
+InternalText = GreekText.read()
 
 MyList = [
+
+#Prep υ > Φ
+('Αυ','Αφ'),('αυ','αφ'),('Ευ','εφ'),('ευ','εφ'),('Ηυ','Ηφ'),('ηυ','ηφ'),
+('Aύ','Άφ'),('αύ','άφ'),('Εύ','Έφ'),('εύ','έφ'),('Ηύ','Ήφ'),('ηύ','ήφ'),
+
+#Prep υ > u
+('Ου','Ou'),('ου','ou'),
+('Ού','Oú'),('ού','oú'),
+
+#Greek to latin
+('Α','A'),('α','a'),('Β','V'),('β','v'),('Γ','G'),('γ','g'),('Δ','D'),('δ','d'),
+('Ε','E'),('ε','e'),('Ζ','Z'),('ζ','z'),('Η','I'),('η','i'),('Θ','Th'),('θ','th'),
+('Ι','I'),('ι','i'),('Κ','K'),('κ','k'),('Λ','L'),('λ','l'),('Μ','M'),('μ','m'),
+('Ν','N'),('ν','n'),('Ξ','Ks'),('ξ','ks'),('Ο','O'),('ο','o'),('Π','P'),('π','p'),
+('Ρ','R'),('ρ','r'),('Σ','S'),('σ','s'),('ς','s'),('Τ','T'),('τ','t'),('Υ','I'),
+('υ','i'),('Φ','F'),('φ','f'),('Χ','Ch'),('χ','ch'),('Ψ','Ps'),('ψ','ps'),
+('Ω','o'),('ω','o'),
+
+#Accented vowels
+('Ά','Á'),('Ή','Í'),('Έ','É'),('Ί','Í'),('Ό','Ó'),('Ύ','Ý'),('Ώ','Ó'),
+('ά','á'),('ή','í'),('έ','é'),('ί','í'),('ό','ó'),('ύ','ý'),('ώ','ó'),
+
+#post conversion clean-up
 ('ai','e'),('ou','u'),('ei','i'),('nt','d')
 ,('oi','i'),('eí','í'),('oú','ú'),('mp','b')
 ,('oí','í'),('aí','é'),('y','i'),('ý','í')
@@ -12,9 +36,16 @@ MyList = [
 ,('ge','ye'),('Ge','Ye'),('gi','yi'),('gé','yé')
 ,('Gi','Yi'),('gí','yí'),('dh','nth'),('prí','proí')
 ,('edh','enth'),(' zí ',' zoí '),('X','Ks')
-,('Dimítris: ','\n\nDimítris: \n'),('Mariléna: ','\n\nMarilú: \n')]
+,('Mariléna','Marilú')]
 
 for k,v in MyList:
-    text = text.replace(k, v)
+    InternalText = InternalText.replace(k, v)
 
-print ("\n\n\n"), text
+GreekText.close()
+
+#Write GreekText.txt file with transliterated Greek. #Need to close and open
+#GreekText again.
+
+WriteFile = open("GreekText.txt", "w")
+WriteFile.write(InternalText)
+WriteFile.close()
--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Greek-Transliterator
-A code transliterating the Greek script into Roman letters.
+A code transliterating the Greek script into a reader-friendly Roman letter transcription.
 
-A work in progress. Currently only modifying Google Translate Greek Transliteration to make it more reader-friendly.
+A work in progress.
 
-Ex.:
+Currently capable of doing this:
 
-opoía > opía
-aftoús > aftús
-kai > ke
+Input:
+
+Ο Όμηρος φέρεται ως ο δημιουργός του ποιητικού έργου της Ιλιάδας, από τα πρώτα κείμενα της Ιστορικής περιόδου της αρχαίας Ελλάδας, γνωστά ως «Ομηρικά Έπη».
+
+Output:
+
+O Ómiros férete os o dimiurgós tu piitikú érgu tis Iliádas, apó ta próta kímena tis Istorikís periódu tis archéas Elládas, gnostá os «Omiriká Épi».
+
+How to use:
+
+1) Clone the code to your computer
+2) Create the file 'GreekText.txt' in the folder where Main.py is located.
+3) Copy-paste the Greek text into 'GreekText.txt' and save.
+4) Run the code.
+5) The code will overwrite 'GreekText.txt' with a Romanized version of the Greek text. 
